c0813b05232eb4cfb3003a7dc4e678a0ec055d95
rosieljdawes <rosieljdawes@gmail.com>
2024-06-04 17:19:26 +0100

    corrected x_in calculation and defined certainties, edited some values

--- wildcats_finances.py
+++ wildcats_finances.py
@@ -1,12 +1,12 @@
 # variables:
 # both variables:
-num_members = 71 
-matches_per_year_per_team = 5
+num_members = 71 # certain
+matches_per_year_per_team = 5 # certain
 mpypt = matches_per_year_per_team
 # money in variables:
-sem_1_ice_fee_paying_members = 28 # unsure
+sem_1_ice_fee_paying_members = 26 # unsure
 s1ifpm = sem_1_ice_fee_paying_members
-sem_2_ice_fee_paying_members = 28 # unsure
+sem_2_ice_fee_paying_members = 29 # unsure
 s2ifpm = sem_2_ice_fee_paying_members
 players = 11 # average from this year- get stats
 membership_fee = 50 # certain
@@ -22,20 +22,20 @@ beginners_paying_sem_1_beg_ice_fee = 2 # unsure
 bps1bif = beginners_paying_sem_1_beg_ice_fee
 beginners_paying_sem_2_beg_ice_fee = 2 # unsure
 bps2bif = beginners_paying_sem_2_beg_ice_fee
-grant = 2350
-spon = 0
-match_fee_cost = 20
+grant = 2350 # unsure
+spon = 0 # for these purposes, set at zero
+match_fee_cost = 20 # certain
 # percent_of_members_pay_ice_fees = (s1ifpm + s2ifpm) / num_members
 # pompif = percent_of_members_pay_ice_fees
 
 # money out variables:
 num_training_sessions = 40 # also a guess
-ice_hire = 120
+ice_hire = 120 # certai
 ref_hire = 130 # 90-140
 num_beginners_sessions = 20 #also unsure by a bit
-num_teams = 2
-buiha_fee = 100
-buiha_aff_fee = 50
+num_teams = 2 #certain
+buiha_fee = 100 # certain
+buiha_aff_fee = 50 # certain
 
 
 
@@ -58,7 +58,7 @@ beginners_costs = num_beginners_sessions * ice_hire
 buiha_costs = buiha_aff_fee + (buiha_fee * num_teams)
 
 
-x_in = membership_fee + grant + spon + annual_ice_fees + match_fees_income
+x_in = membership_income + grant + spon + annual_ice_fees + match_fees_income
 x_out = training_cost + match_cost + beginners_costs + buiha_costs
 
 
